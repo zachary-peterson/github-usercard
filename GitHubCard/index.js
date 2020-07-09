e53b77a6020d5744b173805a987cf648e68a77f4
Zachary Peterson <zp.lambda@yahoo.com>
2020-07-09 18:39:20 -0400

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,205 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import Axios from 'Axios';
+
+const buildIt = document.querySelector('.cards');
+const myGitRepos = 'https://api.github.com/users/zachary-peterson';
+const runWithThis = {
+  "login": "zachary-peterson",
+  "id": 15370405,
+  "node_id": "MDQ6VXNlcjE1MzcwNDA1",
+  "avatar_url": "https://avatars0.githubusercontent.com/u/15370405?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/zachary-peterson",
+  "html_url": "https://github.com/zachary-peterson",
+  "followers_url": "https://api.github.com/users/zachary-peterson/followers",
+  "following_url": "https://api.github.com/users/zachary-peterson/following{/other_user}",
+  "gists_url": "https://api.github.com/users/zachary-peterson/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/zachary-peterson/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/zachary-peterson/subscriptions",
+  "organizations_url": "https://api.github.com/users/zachary-peterson/orgs",
+  "repos_url": "https://api.github.com/users/zachary-peterson/repos",
+  "events_url": "https://api.github.com/users/zachary-peterson/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/zachary-peterson/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Zachary Peterson",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 21,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2015-10-28T22:23:44Z",
+  "updated_at": "2020-06-29T15:26:20Z"
+}
+const listRunWithThis =[
+  {
+    "login": "zachary-peterson",
+    "id": 15370405,
+    "node_id": "MDQ6VXNlcjE1MzcwNDA1",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/15370405?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/zachary-peterson",
+    "html_url": "https://github.com/zachary-peterson",
+    "followers_url": "https://api.github.com/users/zachary-peterson/followers",
+    "following_url": "https://api.github.com/users/zachary-peterson/following{/other_user}",
+    "gists_url": "https://api.github.com/users/zachary-peterson/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/zachary-peterson/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/zachary-peterson/subscriptions",
+    "organizations_url": "https://api.github.com/users/zachary-peterson/orgs",
+    "repos_url": "https://api.github.com/users/zachary-peterson/repos",
+    "events_url": "https://api.github.com/users/zachary-peterson/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/zachary-peterson/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Zachary Peterson",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 21,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2015-10-28T22:23:44Z",
+    "updated_at": "2020-06-29T15:26:20Z"
+  },
+  {
+    "login": "zachary-peterson",
+    "id": 15370405,
+    "node_id": "MDQ6VXNlcjE1MzcwNDA1",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/15370405?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/zachary-peterson",
+    "html_url": "https://github.com/zachary-peterson",
+    "followers_url": "https://api.github.com/users/zachary-peterson/followers",
+    "following_url": "https://api.github.com/users/zachary-peterson/following{/other_user}",
+    "gists_url": "https://api.github.com/users/zachary-peterson/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/zachary-peterson/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/zachary-peterson/subscriptions",
+    "organizations_url": "https://api.github.com/users/zachary-peterson/orgs",
+    "repos_url": "https://api.github.com/users/zachary-peterson/repos",
+    "events_url": "https://api.github.com/users/zachary-peterson/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/zachary-peterson/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Zachary Peterson",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 21,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2015-10-28T22:23:44Z",
+    "updated_at": "2020-06-29T15:26:20Z"
+  },
+  {
+    "login": "zachary-peterson",
+    "id": 15370405,
+    "node_id": "MDQ6VXNlcjE1MzcwNDA1",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/15370405?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/zachary-peterson",
+    "html_url": "https://github.com/zachary-peterson",
+    "followers_url": "https://api.github.com/users/zachary-peterson/followers",
+    "following_url": "https://api.github.com/users/zachary-peterson/following{/other_user}",
+    "gists_url": "https://api.github.com/users/zachary-peterson/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/zachary-peterson/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/zachary-peterson/subscriptions",
+    "organizations_url": "https://api.github.com/users/zachary-peterson/orgs",
+    "repos_url": "https://api.github.com/users/zachary-peterson/repos",
+    "events_url": "https://api.github.com/users/zachary-peterson/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/zachary-peterson/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Zachary Peterson",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 21,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2015-10-28T22:23:44Z",
+    "updated_at": "2020-06-29T15:26:20Z"
+  },
+  {
+    "login": "zachary-peterson",
+    "id": 15370405,
+    "node_id": "MDQ6VXNlcjE1MzcwNDA1",
+    "avatar_url": "https://avatars0.githubusercontent.com/u/15370405?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/zachary-peterson",
+    "html_url": "https://github.com/zachary-peterson",
+    "followers_url": "https://api.github.com/users/zachary-peterson/followers",
+    "following_url": "https://api.github.com/users/zachary-peterson/following{/other_user}",
+    "gists_url": "https://api.github.com/users/zachary-peterson/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/zachary-peterson/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/zachary-peterson/subscriptions",
+    "organizations_url": "https://api.github.com/users/zachary-peterson/orgs",
+    "repos_url": "https://api.github.com/users/zachary-peterson/repos",
+    "events_url": "https://api.github.com/users/zachary-peterson/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/zachary-peterson/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Zachary Peterson",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 21,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2015-10-28T22:23:44Z",
+    "updated_at": "2020-06-29T15:26:20Z"
+  }
+]
+
+// Axios.get(myGitRepos)
+// .then(function(suc){
+//   console.log(suc);
+//   // gitReposWork = suc;
+//   gitCardMaker(myGitRepos);
+// })
+// .catch(function(err){
+//   console.log(err);
+// })
+
+// console.log(Axios);
+
+// Axios.get(followersArray)
+// .then(function(suc){
+//   gitMultiCardMaker(suc);
+//   console.log('It worked')
+// })
+// .catch(function(err){
+//   debugger
+//   console.log('err');
+// })
+// debugger
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +227,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [{'https://api.github.com/users/tetondan'}, {'https://api.github.com/users/dustinmyers'}, {'https://api.github.com/users/justsml'}, {'https://api.github.com/users/luishrd'}, {'https://api.github.com/users/bigknell'}];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,6 +249,51 @@ const followersArray = [];
     </div>
 */
 
+function gitCardMaker(object){
+  const theCard = document.createElement('div');
+  const cardImage = document.createElement('img');
+  const cardInfoDiv = document.createElement('div');
+  const cardHeading = document.createElement('h3');
+  const cardUserName = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  const cardProfile = document.createElement('p');
+  const cardLink = document.createElement('a')
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+
+  theCard.appendChild(cardImage);
+  theCard.appendChild(cardInfoDiv);
+  cardInfoDiv.appendChild(cardHeading);
+  cardInfoDiv.appendChild(cardUserName);
+  cardInfoDiv.appendChild(cardLocation);
+  cardInfoDiv.appendChild(cardProfile);
+  cardProfile.appendChild(cardLink);
+  cardInfoDiv.appendChild(cardFollowers);
+  cardInfoDiv.appendChild(cardFollowing);
+  cardInfoDiv.appendChild(cardBio);
+  
+
+  theCard.classList.add('card');
+  cardInfoDiv.classList.add('card-info');
+  cardHeading.classList.add('name');
+  cardUserName.classList.add('username');
+
+  cardImage.src = object['avatar_url'];
+  cardHeading.textContent = `${object.name}`;
+  cardUserName.textContent = `${object.login}`;
+  cardLocation.textContent = `Location: ${object.location}`;
+  cardProfile.innerHTML = `Profile: <a href="${object['html_url']}">${object['html_url']}</a>`;
+
+  cardFollowers.textContent = `Followers: ${object.followers}`;
+  cardFollowing.textContent = `Following: ${object.following}`;
+  cardBio.textContent = `Bio: ${object.bio}`;
+
+  return theCard;
+}
+
+// buildIt.appendChild(gitCardMaker(runWithThis));
+
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +302,51 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+function gitMultiCardMaker(array){
+  array.forEach(obj => {
+    const theCard = document.createElement('div');
+    const cardImage = document.createElement('img');
+    const cardInfoDiv = document.createElement('div');
+    const cardHeading = document.createElement('h3');
+    const cardUserName = document.createElement('p');
+    const cardLocation = document.createElement('p');
+    const cardProfile = document.createElement('p');
+    const cardLink = document.createElement('a')
+    const cardFollowers = document.createElement('p');
+    const cardFollowing = document.createElement('p');
+    const cardBio = document.createElement('p');
+
+    theCard.appendChild(cardImage);
+    theCard.appendChild(cardInfoDiv);
+    cardInfoDiv.appendChild(cardHeading);
+    cardInfoDiv.appendChild(cardUserName);
+    cardInfoDiv.appendChild(cardLocation);
+    cardInfoDiv.appendChild(cardProfile);
+    cardProfile.appendChild(cardLink);
+    cardInfoDiv.appendChild(cardFollowers);
+    cardInfoDiv.appendChild(cardFollowing);
+    cardInfoDiv.appendChild(cardBio);
+    
+
+    theCard.classList.add('card');
+    cardInfoDiv.classList.add('card-info');
+    cardHeading.classList.add('name');
+    cardUserName.classList.add('username');
+
+    cardImage.src = obj['avatar_url'];
+    cardHeading.textContent = `${obj.name}`;
+    cardUserName.textContent = `${obj.login}`;
+    cardLocation.textContent = `Location: ${obj.location}`;
+    cardProfile.innerHTML = `Profile: <a href="${obj['html_url']}">${obj['html_url']}</a>`;
+
+    cardFollowers.textContent = `Followers: ${obj.followers}`;
+    cardFollowing.textContent = `Following: ${obj.following}`;
+    cardBio.textContent = `Bio: ${obj.bio}`;
+
+    buildIt.appendChild(theCard);
+  })
+}
+
+gitMultiCardMaker(listRunWithThis);
+// gitMultiCardMaker(playingAround);
